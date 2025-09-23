e337158102b3277dfc5ec46ee3e9416272c6ba55
Claus Kadelka <ckadelka@iastate.edu>
2025-09-23 15:11:49 -0500

    updated init

--- boolforge/__init__.py
+++ boolforge/__init__.py
@@ -2,6 +2,7 @@ from boolforge.utils import *
 from boolforge.generate import *
 from boolforge.boolean_function import *
 from boolforge.boolean_network import *
+from boolforge.bio_models import *
 
 try:
     from boolforge._version import __version__
