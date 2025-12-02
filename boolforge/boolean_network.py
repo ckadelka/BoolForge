4f54e1e667dc9632180d5c18e04daad1be8f60b3
Claus Kadelka <ckadelka@iastate.edu>
2025-12-02 12:16:24 -0600

    minor updates to tutorial

--- boolforge/boolean_network.py
+++ boolforge/boolean_network.py
@@ -677,6 +677,7 @@ class BooleanNetwork(WiringDiagram):
                 self.F.append(f)
             else:
                 raise TypeError(f"F holds invalid data type {type(f)} : Expected either list, np.array, or BooleanFunction")
+            assert self.F[ii].n == self.indegrees[ii], f"Index {ii}: Mismatch between the degree of the provided function {self.F[ii].n} and the degree of the wiring diagram {self.indegrees[ii]}."
         if not hasattr(self, 'constants'): #keeps track of all constants and nodes set to constants
             self.constants = {}
         if self.N_constants > 0:
